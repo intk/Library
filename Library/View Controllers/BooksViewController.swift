08187999eaf2efea61d1bd38a9958d7339fe2a67
André Gonçalves <andreslb1@gmail.com>
2016-02-12 18:10:19 +0100

    Add Authors, Books view controller

---
+++ Library/View Controllers/BooksViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BooksViewController.swift
+//  Library
+//
+//  Created by Andre Goncalves on 12/02/16.
+//  Copyright © 2016 INTK. All rights reserved.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController {
+    
+    var author: [String: AnyObject]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
